5216ba75e1001ddb83d0aa83f0cce51a572eecfe
hannahhan153 <hannah.han@biola.edu>
2022-03-24 13:44:49 -0700

    adding seed files

--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,13 @@
 const seedUsers = require('./user-seeds.js');
+// name of the function
+// const seedGuitars = require('./user-guitars.js')
 
 const sequelize = require('../config/connection')
 
 async function seedAll() {
     await sequelize.sync({ force: true });
     console.log('Database synced');
+
+    await seedUsers();
+    console.log('Users seeded');
 }
