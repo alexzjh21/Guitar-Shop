8e63933cda879ad7edbeb1f818e1422a5ca66c92
hannahhan153 <hannah.han@biola.edu>
2022-03-24 14:46:24 -0700

    add seeds files for user and guitar, install mysql2, dotenv, sequelize

--- seeds/user-guitars.js
+++ seeds/user-guitars.js
@@ -6,22 +6,26 @@ const guitarData = [
     {
         guitar_type: 'acoustic',
         guitar_brand: 'Blueridge',
-        
+        price: 1299.99,
+        comment_text:'Pre-War Series BR-260A Dreadnought Acoustic Guitar Natural'
     },
     {
         guitar_type: 'electric',
         guitar_brand: 'Gibson Les Paul',
-        
+        price: 1389.99,
+        comment_text:'Heritage Cherry Sunburst'
     },
     {
         guitar_type: 'bass',
         guitar_brand: 'Ibanez',
-        
+        price: 489.99,
+        comment_text:'Pre-War Series BR-260A Dreadnought Acoustic Guitar Natural'
     },
     {
         guitar_type: 'acoustic',
         guitar_brand: 'Seagull',
-        comment_text:''
+        price: 299.99,
+        comment_text:'S6 Original Acoustic Guitar Natural'
     },
 ];
 
