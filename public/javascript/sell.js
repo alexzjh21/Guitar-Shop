6592152af8ed7fe7fb6e9f4779b5517a20a7417f
alexzjh21 <alexzjh21@yahoo.co.jp>
2022-03-27 21:22:37 -0400

    fixed sell function

--- public/javascript/sell.js
+++ public/javascript/sell.js
@@ -1,18 +1,18 @@
 async function sellFormHandler (event) {
     event.preventDefault();
 
-    const type = document.querySelector('#guitar-type');
-    const brand = document.querySelector('#guitar-brand');
-    const description = document.querySelector('#description');
-    const price = document.querySelector('#price');
+    const type = document.querySelector('#guitar-type').value;
+    const brand = document.querySelector('#guitar-brand').value.trim();
+    const comment_text = document.querySelector('#comment_text').value;
+    const price = document.querySelector('#price').value.trim();
 
     if (type && brand && description && price) {
-        const response = await fetch('/api/sell', {
+        const response = await fetch('/api/guitars', {
           method: 'POST',
           body: JSON.stringify({
             type,
             brand,
-            description,
+            comment_text,
             price
           }),
           headers: { 'Content-Type': 'application/json' }
