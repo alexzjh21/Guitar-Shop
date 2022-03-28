26153d34b969b3edc13513fa913f3f65d1b3f6be
hannahhan153 <hannah.han@biola.edu>
2022-03-27 18:43:11 -0700

    add post route for /logout, destroy method to clear session, add logout button, create logout.js file, link to main handlebars, add click event handler that calls /logout route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -83,6 +83,17 @@ router.post('/login', (req, res) => {
         });
     });
 });
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+}
+});
 // update the password
 router.put('/:id', (req, res) => {
     User.update(req.body, {
